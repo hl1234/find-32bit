5f762e6b4a30cb171b27eae240dd4f4a22207dee
hl1234 <harry.levinson@gmail.com>
2020-03-28 21:04:46 -0400

    Initial file

---
+++ find-32bit.sh
@@ -0,0 +1,28 @@
+#!/bin/bash
+
+starthere="/Applications/KompoZer.app"
+starthere="/Applications /Library ~/Applications ~/Library "
+
+sudo find $starthere -type d -name \*.app |  \
+    while read app ; do
+	appname=`basename "$app"`
+	echo "$appname:$app"
+    done | sort | \
+    while read x ; do
+	appname=`echo "$x" | sed 's/:.*$//'`
+	app=`echo "$x" | sed 's/^.*://'`
+	
+        [ -d "$app"/Contents ] && ( cd "$app"/Contents
+	if [ -f Info.plist ] ; then
+	    exe=`plutil -p Info.plist | awk '/CFBundleExecutable/' | sed 's/^.*=> *//' | sed 's/"//g'`
+	    ft=`file MacOS/"$exe" |grep -q i386 && echo "i386"`
+	    if [ "$ft" = "i386" ] ;then
+		echo "$appname"
+		echo "      $app"
+		echo "      $exe"
+	    fi
+	fi
+	
+	)
+	
+    done
